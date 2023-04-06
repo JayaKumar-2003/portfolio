758626e0fe803e4f33a65ac63f8a17799dc36d8f
JayaKumar-2003 <jayakumar112234@gmail.com>
2023-04-06 16:49:54 +0530

    projects

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -2,19 +2,24 @@ import React from 'react';
 import './Portfolio.css';
 import calculator from '../../img/calculator.png';
 import amazona from '../../img/amazona.png';
-import Carousel from 'react-bootstrap/Carousel'
+import Carousel from 'react-bootstrap/Carousel';
+import TicTacToe from '../../img/tic-tac-toe.png';
 export function Portfolio() {
+
+ 
+
     return (
         <div className='portfolio'>
             <span>Recent Project</span>
             <span>Portfolio</span>
     <Carousel>
       <Carousel.Item className='image'>
-        <img
+        <a href="https://jayakumar-translator.onrender.com/" target='blank'>
+        <img 
           className="d-block w-100"
           src={calculator}
           alt="First slide"
-        />
+        ></img></a>
       </Carousel.Item>
       <Carousel.Item>
         <img
@@ -22,6 +27,16 @@ export function Portfolio() {
           src={amazona}
           alt="Second slide"
         />
+      </Carousel.Item>
+      <Carousel.Item>
+        <a href='https://jayakumar-tictactoe.onrender.com' target='blank'>
+          <img
+           className="d-block w-100"
+           src={TicTacToe}
+           alt="Third slide"
+          >
+          </img>
+        </a>
       </Carousel.Item>
     </Carousel>
         </div>
